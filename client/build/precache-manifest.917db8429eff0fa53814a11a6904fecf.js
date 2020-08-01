7b2efdf8cf4cd89bda5e7cb521d3e1039145fec9
BenjaminZhou <calmzhou@gmail.com>
2020-07-31 17:29:36 -0700

    add build

---
+++ client/build/precache-manifest.917db8429eff0fa53814a11a6904fecf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75380ad2b30eaf03d296886f782bfb1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "241a23fec7ac941d6800",
+    "url": "/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "4a4395ce9b95ad3de2fc",
+    "url": "/static/css/main.2f55372d.chunk.css"
+  },
+  {
+    "revision": "241a23fec7ac941d6800",
+    "url": "/static/js/2.d41d21be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d41d21be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4395ce9b95ad3de2fc",
+    "url": "/static/js/main.7165adc7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
